9d9e2ab60a3f966500e83af662a6420fa1283783
Adrian Aulbach <adrian@aulbach.ch>
2016-01-20 20:02:26 +0100

    Removed "implements ProtocolImpl" in LoginSceneController

--- EasyChatClient/src/main/java/bfh/easychat/client/fx/LoginSceneController.java
+++ EasyChatClient/src/main/java/bfh/easychat/client/fx/LoginSceneController.java
@@ -14,7 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class LoginSceneController extends ProtocolImpl {
+public class LoginSceneController {
 
     private Stage dialogStage;
     private boolean okClicked;
